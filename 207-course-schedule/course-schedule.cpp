cae88ea56723b2dd3c23c87fb1c3a2efbe1ab84a
ADITYA <adityamatya432@gmail.com>
2026-08-05 22:40:41 +0530

    Time: 3 ms (75.19%) | Memory: 18.6 MB (95.07%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        int n = numCourses;
+        int s = pre.size();
+        vector<int>adj[n];
+        for(int i = 0;i<s;i++){
+            int u = pre[i][0];
+            int v =  pre[i][1];
+            adj[v].push_back(u);
+        }
+        vector<int>inDeg(n,0);
+        for(int i = 0;i<n;i++){
+            for(auto it:adj[i]){
+                inDeg[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i = 0;i<n;i++){
+            if(inDeg[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>arr;
+        while(!q.empty()){
+            int node = q.front();
+            arr.push_back(node);
+            q.pop();
+            for(auto it:adj[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+
+        int size = arr.size();
+        if(size==n) return true;
+        return false;
+    }
+};
