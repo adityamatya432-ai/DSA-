0b96a57b4243e82f5aa9c4766baff5bc71ee242e
ADITYA <adityamatya432@gmail.com>
2026-08-06 19:59:51 +0530

    Time: 54 ms (29.49%) | Memory: 65.5 MB (53.35%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
-
-        if (state[node] == 1)
-            return false;          // Cycle found
-
-        if (state[node] == 2)
-            return true;           // Already known to be safe
-
-        if (state[node] == 3)
-            return false;          // Already known to be unsafe
-
-        state[node] = 1;           // Mark as visiting
-
-        for (int nei : graph[node]) {
-            if (!dfs(nei, graph, state)) {
-                state[node] = 3;   // Unsafe
-                return false;
+    bool dfs(vector<int>adj[],vector<int>&safe,vector<int>&visit,vector<int>&pathVisit,int node){
+        visit[node]=1;
+        pathVisit[node]=1;
+        for(auto it:adj[node]){
+            if(!visit[it]){
+                if(dfs(adj,safe,visit,pathVisit,it)==true)return true;
             }
+            else if(pathVisit[it]==1)return true;
         }
-
-        state[node] = 2;           // Safe
-        return true;
+        safe[node]=1;
+        pathVisit[node]=0;
+        return false;
     }
 
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-
         int n = graph.size();
-        vector<int> state(n, 0);
-        vector<int> ans;
-
-        for (int i = 0; i < n; i++) {
-            if (dfs(i, graph, state))
-                ans.push_back(i);
+        vector<int>adj[n];
+        for(int i = 0;i<n;i++){
+            for(auto it:graph[i]){
+                adj[i].push_back(it);
+            }
         }
-
+        vector<int>visit(n,0);
+        vector<int>pathVisit(n,0);
+        vector<int>safe(n,0);
+        for(int i = 0;i<n;i++){
+            if(!visit[i]){
+                dfs(adj,safe,visit,pathVisit,i);
+            }
+        }
+        vector<int>ans;
+        for(int i = 0;i<n;i++){
+            if(safe[i]==1){
+                ans.push_back(i);
+            }
+        }
         return ans;
     }
 };
