79fc193338b3ed1c3e4e6bac603b24f5d21970f1
ADITYA <adityamatya432@gmail.com>
2026-08-07 06:49:35 +0530

    Time: 54 ms (29.56%) | Memory: 64.3 MB (53.67%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
-    bool dfs(vector<int>adj[],vector<int>&safe,vector<int>&visit,vector<int>&pathVisit,int node){
-        visit[node]=1;
-        pathVisit[node]=1;
-        for(auto it:adj[node]){
-            if(!visit[it]){
-                if(dfs(adj,safe,visit,pathVisit,it)==true)return true;
-            }
-            else if(pathVisit[it]==1)return true;
-        }
-        safe[node]=1;
-        pathVisit[node]=0;
-        return false;
-    }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int>adj[n];
+        vector<int>inDeg(n,0);
         for(int i = 0;i<n;i++){
             for(auto it:graph[i]){
-                adj[i].push_back(it);
+                adj[it].push_back(i);
+                inDeg[i]++;
             }
         }
         vector<int>visit(n,0);
-        vector<int>pathVisit(n,0);
-        vector<int>safe(n,0);
+        queue<int>q;
         for(int i = 0;i<n;i++){
-            if(!visit[i]){
-                dfs(adj,safe,visit,pathVisit,i);
+            if(inDeg[i]==0){
+                q.push(i);
+                visit[i]=1;
             }
         }
         vector<int>ans;
-        for(int i = 0;i<n;i++){
-            if(safe[i]==1){
-                ans.push_back(i);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                inDeg[it]--;
+                if(inDeg[it]==0)q.push(it);
             }
         }
+        sort(ans.begin(),ans.end());
         return ans;
+      
     }
 };
