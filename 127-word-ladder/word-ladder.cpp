4a70750e349dcc47db21e41d339eeaa378f71d37
ADITYA <adityamatya432@gmail.com>
2026-08-24 17:45:09 +0530

    Time: 1661 ms (5.01%) | Memory: 54.3 MB (5.07%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -7,24 +7,23 @@ public:
         queue<pair<string,int>>q;
         q.push({beginWord,1});
         st.erase(beginWord);
-        int ans = 0;
         while(!q.empty()){
             string word = q.front().first;
             int d = q.front().second;
             q.pop();
-            if(word==endWord)ans=d;
+            if(word==endWord)return d;
             for(int i = 0;i<word.size();i++){
                 char original = word[i];
                 for(char ch = 'a';ch<='z';ch++){
                     word[i]=ch;
                     if(st.find(word)!=st.end()){
-                        st.erase(word);
                         q.push({word,d+1});
                     }
                 }
                 word[i]=original;
+                st.erase(word);
             }
         }
-        return ans;
+        return 0;
     }
 };
