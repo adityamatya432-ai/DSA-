5547575748073d842c74272cbfc24b72df51265d
ADITYA <adityamatya432@gmail.com>
2026-08-27 16:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.97%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-
-    void reverse(string &s, int st , int end){
-        string temp = s;
-        int j = st;
-        for(int i = end;i>=st;i--){
-            temp[i]=s[j];
-            j++;
-        }
-        s=temp;
-    }
     string reverseWords(string s) {
-        int n = s.size();
         int i = 0;
-        int j = 0;
-        while(j<=n){
-            if(j==n-1){
-                reverse(s,i,j);
+        
+        while (i < s.size()) {
+            int j = i;
+
+            // Find the end of the current word
+            while (j < s.size() && s[j] != ' ') {
+                j++;
             }
-            else if(s[j]==' '){
-                reverse(s,i,j-1);
-                i=j+1;
-            }
-            j++;
+
+            // Reverse the current word
+            reverse(s.begin() + i, s.begin() + j);
+
+            // Move i to the next word
+            i = j + 1;
         }
+
         return s;
     }
 };
