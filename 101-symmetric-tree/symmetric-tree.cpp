b4c8e8237dd3dfc20ac65dfe55609ece4e35c246
ADITYA <adityamatya432@gmail.com>
2026-08-30 11:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (42.50%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,20 @@
+bool check(TreeNode* left, TreeNode* right) {
+    if(left == NULL && right == NULL)
+        return true;
+
+    if(left == NULL || right == NULL)
+        return false;
+
+    if(left->val != right->val)
+        return false;
+
+    return check(left->left, right->right) &&
+           check(left->right, right->left);
+}
+
+class Solution{
+public:
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left, root->right);
+    }
+};
